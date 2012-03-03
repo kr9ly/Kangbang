5ce131c2a3cdbf9d4ffb89ff85cb3f1a32bcfc61
karakuri <s@umecha.biz>
2012-03-03 10:44:30 +0900

    - remove extra __callStatic method

--- common/view.php
+++ common/view.php
@@ -1,16 +1,8 @@
 <?php
 class View {
+	/* static start */
 	private static $instances = array();
 	private static $activeView;
-
-	/* static start */
-	public static function __callstatic($method, $argument) {
-		if (!array_key_exists(get_called_class(), self::$instances)) {
-			self::$instances[get_called_class()] = new static();
-		}
-		self::$activeView = self::$instances[get_called_class()];
-		return call_user_func_array(array(self::$activeView,$method), $argument);
-	}
 
 	public static function get() {
 		if (!array_key_exists(get_called_class(), self::$instances)) {
