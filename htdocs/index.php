4f3a0cf39c3457f6866ae085a8930cc81d86c00b
karakuri <s@umecha.biz>
2012-03-05 23:34:58 +0900

    + change view path base loading

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,16 +1,12 @@
 <?php
 require '../common/common.php';
 
+View::getByPath($_GET['q']);
+
 $view = Controller::execByPath($_GET['q']);
+
 if ($view) {
 	$view->display();
 } else {
-	if (!View::displayView()) {
-		$view = View::getByPath($_GET['q']);
-		if ($view) {
-			$view->display();
-		} else {
-			die('404 not found');
-		}
-	}
+	View::displayView();
 }
