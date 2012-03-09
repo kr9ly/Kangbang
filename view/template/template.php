0ff031096cf680b29bf52730d9e05be35d267a01
karakuri <s@umecha.biz>
2012-03-09 11:34:33 +0900

    + add template escaped val getter

--- view/template/template.php
+++ view/template/template.php
@@ -14,9 +14,39 @@ class TemplateView extends View {
 	public function display() {
 		$template = file_get_contents(BASE_PATH . $this->path . '.tpl.php');
 		ob_start();
-		extract($this->params);
 		eval('?>' . $template);
 		echo ob_get_clean();
+	}
+	
+	public function __get($key) {
+		if (strpos($key,'_')) {
+			return $this->params[$key];
+		}
+		return htmlspecialchars($this->params[$key]);
+	}
+	
+	private function get($key) {
+		return htmlspecialchars($_GET[$key]);
+	}
+	
+	private function post($key) {
+		return htmlspecialchars($_POST[$key]);
+	}
+	
+	private function request($key) {
+		return htmlspecialchars($_REQUEST[$key]);
+	}
+	
+	private function _get($key) {
+		return $_GET[$key];
+	}
+	
+	private function _post($key) {
+		return $_POST[$key];
+	}
+	
+	private function request($key) {
+		return $_REQUEST[$key];
 	}
 
 }
