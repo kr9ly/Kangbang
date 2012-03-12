fedd071febb988c54ecd34806845c585c8521a30
karakuri <s@umecha.biz>
2012-03-12 21:36:27 +0900

    + change dir structure + remove extra alert class

--- parts/common/alert/alert.php
+++ parts/common/alert/alert.php
@@ -1,5 +1,5 @@
 <?php
-class SuccessAlertCommonParts extends Parts {
+class AlertCommonParts extends Parts {
 	public function exec($text) {
 		TemplateView::get()->setParam('text',$text);
 	}
--- parts/common/alert/error.php
+++
@@ -1,6 +0,0 @@
-<?php
-class ErrorAlertCommonParts extends Parts {
-	public function exec($text) {
-		TemplateView::get()->setParam('text',$text);
-	}
-}
