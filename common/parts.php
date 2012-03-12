de2f8df2cb6b85956ccc24b9129c6eb83404d29e
karakuri <s@umecha.biz>
2012-03-12 21:25:56 +0900

    + change parts path resolve

--- common/parts.php
+++ common/parts.php
@@ -2,11 +2,27 @@
 class Parts extends Base {
 	/* static */
 	public static function display($path) {
-		if (is_file(BASE_PATH . '/parts/' . $path . '.php') || is_file(BASE_PATH . '/parts/' . $path . '/' . pathinfo($path,PATHINFO_FILENAME) . '.php')) {
-			$args = func_get_args();
-			array_shift($args);
-			$clName = TextHelper::toCamelCase(implode('_',array_reverse(explode('_',str_replace('/','_',$path))))) . 'Parts';
+		$args = func_get_args();
+		array_shift($args);
+		if (Cache::isExists('parts/' . $path)) {
+			$clName = Cache::get('parts/' . $path);
 			call_user_func_array(array(new $clName, 'exec'), $args);
+		} else {
+			$pathArray = explode('/', $path);
+			while (count($pathArray) > 0) {
+				$temp = implode('/',$pathArray);
+				if (is_file(BASE_PATH . '/parts/' . $temp . '.php') || is_file(BASE_PATH . '/parts/' . $temp . '/' . pathinfo($temp,PATHINFO_FILENAME) . '.php')) {
+					$tempArray = array();
+					foreach ($pathArray as $dir) {
+						$tempArray = array_merge($tempArray,array_reverse(explode('_',$dir)));
+					}
+					$clName = TextHelper::toCamelCase(implode('_', array_reverse($tempArray))) . 'Parts';
+					call_user_func_array(array(new $clName, 'exec'), $args);
+					Cache::set('parts/' . $path,$clName);
+					break;
+				}
+				array_pop($pathArray);
+			}
 		}
 		TemplateView::get()->setPath('/parts/' . $path);
 		View::displayView();
