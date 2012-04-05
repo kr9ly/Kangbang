4147ee3b29fe8454f795a6ba807cbd2f48e55f10
karakuri <s@umecha.biz>
2012-04-05 22:58:18 +0900

    fix controller path resolve bug

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,7 +6,7 @@ View::getByPath($_GET['q']);
 $controller = Controller::getByPath($_GET['q']);
 Filter::setController($controller);
 Filter::before();
-$view = $controller->exec();
+$view = Controller::execByPath($_GET['q']);
 Filter::after();
 
 if ($view) {
