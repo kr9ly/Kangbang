8abe7bd5a8d75ad2d88c7d3fb21a84c037a9d8c8
s_umemori <s_umemori@cross_c.co.jp>
2012-04-23 13:04:25 +0900

    add do all tests method

--- common/test.php
+++ common/test.php
@@ -6,8 +6,8 @@ class Test {
 	public static function assert($function, $assertion, $message, $args = array()) {
 		$trace = debug_backtrace();
 
-		if (!$results[$trace[1]['class']]) {
-			$results[$trace[1]['class']] = array();
+		if (!self::$results[$trace[1]['class']]) {
+			self::$results[$trace[1]['class']] = array();
 		}
 
 		try {
@@ -16,9 +16,62 @@ class Test {
 			if (!$res) {
 				throw new Exception();
 			}
-			$results[$trace[1]['class']][$message] = true;
+			self::$results[$trace[1]['class']][$message] = true;
 		} catch (Exception $e) {
-			$results[$trace[1]['class']][$message] = false;
+			self::$results[$trace[1]['class']][$message] = false;
+		}
+	}
+
+	public static function doAllTests($path = '') {
+		if ($handle = opendir(BASE_PATH . '/' . $path)) {
+			while (false !== ($file = readdir($handle))) {
+				if (strpos($file,'.') === 0) {
+					continue;
+				}
+				if (is_dir(BASE_PATH . '/' . $path . $file) && $file != 'library') {
+					self::doAllTests($path . $file . '/',$init);
+				}
+				if (pathinfo($path . $file,PATHINFO_EXTENSION) == 'php') {
+					$pathArr = explode('/',$path . pathinfo($path . $file,PATHINFO_FILENAME));
+					if (count($pathArr) > 1 && $pathArr[count($pathArr)-1] == $pathArr[count($pathArr)-2]) {
+						array_pop($pathArr);
+					}
+					$tempArr = array();
+					foreach ($pathArr as $val) {
+						$tempArr = array_merge($tempArr,array_reverse(explode('_',$val)));
+					}
+					$tempArr = array_filter($tempArr,'strlen');
+
+					switch ($pathArr[0]) {
+						case 'controller':
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr))) . 'Controller';
+							break;
+						case 'model':
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr))) . 'Dao';
+							break;
+						case 'parts':
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr))) . 'Parts';
+							break;
+						case 'helper':
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr))) . 'Helper';
+							break;
+						case 'view':
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr))) . 'View';
+							break;
+						case 'filter':
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr))) . 'Filter';
+							break;
+						default:
+							$clName = TextHelper::toCamelCase(implode('_',array_reverse($tempArr)));
+							break;
+					}
+
+					if (Loader::classExists($clName) && is_callable($clName . '::unittest')) {
+						call_user_func($clName . '::unittest');
+					}
+				}
+			}
+			closedir($handle);
 		}
 	}
 }
