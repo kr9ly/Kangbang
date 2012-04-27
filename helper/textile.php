36c993d0e0db5e7fd054900d0d361cd8bd891a21
Karakuri <s@umecha.biz>
2012-04-27 15:59:35 +0900

    add link and headword convertion to textile helper

--- helper/textile.php
+++ helper/textile.php
@@ -10,38 +10,50 @@ class TextileHelper extends Helper {
 		$blocktype = false;
 		foreach ($lines as $line) {
 			$length = mb_strlen($line);
-			if ($line == "") {
+			if (strpos($line, 'bq.') === 0) {
+				if ($blocktype) {
+					$res .= "\n</" . $blocktype . ">\n";
+					$blocktype = false;
+				}
+				$blocktype = 'blockquote';
+				$line = substr($line, 3);
+				$res .= "<blockquote>\n";
+			} else if (preg_match("/^p([>=])?(\(+|\)+)?\./u",$line,$matches)) {
+				if ($blocktype) {
+					$res .= "\n</" . $blocktype . ">\n";
+					$blocktype = false;
+				}
+				$blocktype = 'p';
+				if ($matches[1] == '>') {
+					$style = 'text-align:right;';
+				} else if ($matches[1] == '=') {
+					$style = 'text-align:center;';
+				}
+				if (strpos($matches[1], '(') === 0) {
+					$margin = strlen($mathces[1]);
+					$style .= 'margin-left:' . $margin . 'em';
+				} else if (strpos($matches[1], ')') === 0) {
+					$margin = strlen($mathces[1]);
+					$style .= 'margin-right:' . $margin . 'em';
+				}
+				$block = '<p' . ($style ? ' style="' . $style . '"' : '') . '>';
+				$line = substr($line,strlen($block));
+				$res .= $block . "\n";
+			} else if (preg_match("/^h[1-6]\./u",$line,$matches)) {
+				if ($blocktype) {
+					$res .= "\n</" . $blocktype . ">\n";
+					$blocktype = false;
+				}
+				$blocktype = 'h' . $matches[1];
+				$line = substr($line,3);
+				$res .= '<h' . $matches[1] . '>';
+			} else if ($line == "") {
 				$res .= "\n";
 				if ($blocktype) {
 					$res .= '</' . $blocktype . '>';
 					$blocktype = false;
 				}
 				continue;
-			}
-			if (!$blocktype) {
-				$res .= "\n";
-				if (strpos($line, 'bq.') === 0) {
-					$blocktype = 'blockquote';
-					$line = substr($line, 3);
-					$res .= "<blockquote>\n";
-				} else if (preg_match("/^p([>=])?(\(+|\)+)?\./u",$line,$matches)) {
-					$blocktype = 'p';
-					if ($matches[1] == '>') {
-						$style = 'text-align:right;';
-					} else if ($matches[1] == '=') {
-						$style = 'text-align:center;';
-					}
-					if (strpos($matches[1], '(') === 0) {
-						$margin = strlen($mathces[1]);
-						$style .= 'margin-left:' . $margin . 'em';
-					} else if (strpos($matches[1], ')') === 0) {
-						$margin = strlen($mathces[1]);
-						$style .= 'margin-right:' . $margin . 'em';
-					}
-					$block = '<p' . ($style ? ' style="' . $style . '"' : '') . '>';
-					$line = substr($line,strlen($block));
-					$res .= $block . "\n";
-				}
 			} else {
 				$res .= "<br />\n";
 			}
@@ -50,39 +62,51 @@ class TextileHelper extends Helper {
 				$temp = $chr;
 				switch ($chr) {
 					case '*':
-						if (preg_match("/^\*(.+?)\*/u", mb_substr($line, $i), $matches)) {
-							$temp = '<span style="font-weight:bold">' . $matches[1] . '</span>';
-							$i += mb_strlen($matches[1])-1+2;
+						$offset = mb_strpos(mb_substr($line, $i+1), '*');
+						if ($offset !== false) {
+							$temp = '<span style="font-weight:bold">' . mb_substr($line, $i+1, $offset) . '</span>';
+							$i = $offset+1;
 						}
 						break;
 					case '_':
-						if (preg_match("/^_(.+?)_/u", mb_substr($line, $i), $matches)) {
-							$temp = '<span style="font-style:italic">' . $matches[1] . '</span>';
-							$i += mb_strlen($matches[1])-1+2;
+						$offset = mb_strpos(mb_substr($line, $i+1), '_');
+						if ($offset !== false) {
+							$temp = '<span style="font-style:italic">' . mb_substr($line, $i+1, $offset) . '</span>';
+							$i = $offset+1;
 						}
 						break;
 					case '+':
-						if (preg_match("/^\+(.+?)\+/u", mb_substr($line, $i), $matches)) {
-							$temp = '<span style="text-decoration:underline">' . $matches[1] . '</span>';
-							$i += mb_strlen($matches[1])-1+2;
+						$offset = mb_strpos(mb_substr($line, $i+1), '+');
+						if ($offset !== false) {
+							$temp = '<span style="text-decoration:underline">' . mb_substr($line, $i+1, $offset) . '</span>';
+							$i = $offset+1;
 						}
 						break;
 					case '-':
-						if (preg_match("/^-(.+?)-/u", mb_substr($line, $i), $matches)) {
-							$temp = '<span style="text-decoration:line-through">' . $matches[1] . '</span>';
-							$i += mb_strlen($matches[1])-1+2;
+						$offset = mb_strpos(mb_substr($line, $i+1), '-');
+						if ($offset !== false) {
+							$temp = '<span style="text-decoration:line-through">' . mb_substr($line, $i+1, $offset) . '</span>';
+							$i = $offset+1;
 						}
 						break;
 					case '??':
-						if (preg_match("/^\?\?(.+?)\?\?/u", mb_substr($line, $i), $matches)) {
-							$temp = '<span style="font-style:italic">' . $matches[1] . '</span>';
-							$i += mb_strlen($matches[1])-1+4;
+						$offset = mb_strpos(mb_substr($line, $i+2), '??');
+						if ($offset !== false) {
+							$temp = '<span style="font-style:italic">' . mb_substr($line, $i+2, $offset) . '</span>';
+							$i = $offset+2;
 						}
 						break;
 					case '@':
-						if (preg_match("/^@(.+?)@/u", mb_substr($line, $i), $matches)) {
-							$temp = '<span style="font-family: monospace">' . $matches[1] . '</span>';
-							$i += mb_strlen($matches[1])-1+2;
+						$offset = mb_strpos(mb_substr($line, $i+1), '@');
+						if ($offset !== false) {
+							$temp = '<span style="font-family: monospace">' . mb_substr($line, $i+1, $offset) . '</span>';
+							$i = $offset+1;
+						}
+						break;
+					case '"':
+						if (preg_match("/[^ ](\"(.+)\":(.+))[ $]/u", mb_substr($line,$i==0 ? 0 : $i-1), $matches) && filter_var($matches[3],FILTER_VALIDATE_URL)) {
+							$temp = '<a href="' . $matches[3] . '">' . $matches[2] . '</a>';
+							$i += mb_strlen($matches[1]);
 						}
 						break;
 				}
