cef88e7a5111d8dadcb6751a8dda357497869753
Karakuri <s@umecha.biz>
2012-04-27 16:20:23 +0900

    add pre convertion to textile helper

--- helper/textile.php
+++ helper/textile.php
@@ -1,7 +1,5 @@
 <?php
 class TextileHelper extends Helper {
-	private static $converting = true;
-
 	public static function convert($textile) {
 		$res = "";
 
@@ -14,14 +12,25 @@ class TextileHelper extends Helper {
 				if ($blocktype) {
 					$res .= "\n</" . $blocktype . ">\n";
 					$blocktype = false;
+					$converting = true;
 				}
 				$blocktype = 'blockquote';
 				$line = substr($line, 3);
 				$res .= "<blockquote>\n";
+			} else if (strpos($line, 'pre.') === 0) {
+				if ($blocktype) {
+					$res .= "\n</" . $blocktype . ">\n";
+					$blocktype = false;
+					$converting = true;
+				}
+				$blocktype = 'pre';
+				$line = substr($line, 4);
+				$res .= "<pre>\n";
 			} else if (preg_match("/^p([>=])?(\(+|\)+)?\./u",$line,$matches)) {
 				if ($blocktype) {
 					$res .= "\n</" . $blocktype . ">\n";
 					$blocktype = false;
+					$converting = true;
 				}
 				$blocktype = 'p';
 				if ($matches[1] == '>') {
@@ -43,6 +52,7 @@ class TextileHelper extends Helper {
 				if ($blocktype) {
 					$res .= "\n</" . $blocktype . ">\n";
 					$blocktype = false;
+					$converting = true;
 				}
 				$blocktype = 'h' . $matches[1];
 				$line = substr($line,3);
@@ -52,10 +62,14 @@ class TextileHelper extends Helper {
 				if ($blocktype) {
 					$res .= '</' . $blocktype . '>';
 					$blocktype = false;
+					$converting = true;
 				}
 				continue;
 			} else {
 				$res .= "<br />\n";
+			}
+			if (!$converting) {
+				continue;
 			}
 			for ($i=0;$i<$length;$i++) {
 				$chr = mb_substr($line, $i, 1);
@@ -65,48 +79,48 @@ class TextileHelper extends Helper {
 						$offset = mb_strpos(mb_substr($line, $i+1), '*');
 						if ($offset !== false) {
 							$temp = '<span style="font-weight:bold">' . mb_substr($line, $i+1, $offset) . '</span>';
-							$i = $offset+1;
+							$i = $offset;
 						}
 						break;
 					case '_':
 						$offset = mb_strpos(mb_substr($line, $i+1), '_');
 						if ($offset !== false) {
 							$temp = '<span style="font-style:italic">' . mb_substr($line, $i+1, $offset) . '</span>';
-							$i = $offset+1;
+							$i = $offset;
 						}
 						break;
 					case '+':
 						$offset = mb_strpos(mb_substr($line, $i+1), '+');
 						if ($offset !== false) {
 							$temp = '<span style="text-decoration:underline">' . mb_substr($line, $i+1, $offset) . '</span>';
-							$i = $offset+1;
+							$i = $offset;
 						}
 						break;
 					case '-':
 						$offset = mb_strpos(mb_substr($line, $i+1), '-');
 						if ($offset !== false) {
 							$temp = '<span style="text-decoration:line-through">' . mb_substr($line, $i+1, $offset) . '</span>';
-							$i = $offset+1;
+							$i = $offset;
 						}
 						break;
 					case '??':
 						$offset = mb_strpos(mb_substr($line, $i+2), '??');
 						if ($offset !== false) {
 							$temp = '<span style="font-style:italic">' . mb_substr($line, $i+2, $offset) . '</span>';
-							$i = $offset+2;
+							$i = $offset+1;
 						}
 						break;
 					case '@':
 						$offset = mb_strpos(mb_substr($line, $i+1), '@');
 						if ($offset !== false) {
 							$temp = '<span style="font-family: monospace">' . mb_substr($line, $i+1, $offset) . '</span>';
-							$i = $offset+1;
+							$i = $offset;
 						}
 						break;
 					case '"':
-						if (preg_match("/[^ ](\"(.+)\":(.+))[ $]/u", mb_substr($line,$i==0 ? 0 : $i-1), $matches) && filter_var($matches[3],FILTER_VALIDATE_URL)) {
+						if (preg_match("/[^ ](\"(.+?)\":(.+?))[ $]/u", mb_substr($line,$i==0 ? 0 : $i-1), $matches) && filter_var($matches[3],FILTER_VALIDATE_URL)) {
 							$temp = '<a href="' . $matches[3] . '">' . $matches[2] . '</a>';
-							$i += mb_strlen($matches[1]);
+							$i += mb_strlen($matches[1])-1;
 						}
 						break;
 				}
